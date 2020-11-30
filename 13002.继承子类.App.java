35b8c30c0e36f847629221dc04de4d8bb65a58f1
huangyongbo0316 <57880056+huangyongbo0316@users.noreply.github.com>
2020-11-30 12:03:50 +0800

    Update 13002.继承子类.App.java

--- 13002.继承子类.App.java
+++ 13002.继承子类.App.java
@@ -2,8 +2,15 @@ package demo;
 
 public class App implements R{
 	public static void main(String[] args) {
+		System.out.println(R.boy_filePath);
+		System.out.println(R.girl_filePath);
 		
-	R.test();
+		  for (int i = 0; i < array2d.length; i++) { for (int j = 0; j <
+		  array2d[0].length; j++) { System.out.print(array2d[i][j]+" "); }
+		  System.out.println(); }
+		 
+	
 	}
 
 }
+
